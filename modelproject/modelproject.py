21a2c9a6a16a4a66587ad06d9a14d7eb2bb8faf9
simonrippert <simon.rippert@gmail.com>
2023-05-10 10:48:49 +0200

    numerical under autarky

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -17,11 +17,7 @@ class RicardianModelClass:
         a2_g = 3
 
         #default labor shares in Denmark and Germany
-        L1_d = 0.5
-        L2_d = 0.5
         L_d = 1
-        L1_g = 0.7
-        L2_g = 0.3
         L_g = 1
 
         #default wages (guesses)
@@ -36,30 +32,17 @@ class RicardianModelClass:
         c1_g = 1
         c2_g = 1
 
-
-
-    def ricardian_ppf_Denmark(self, a1_d, a2_d, L_d):
-        #production possibility set for Denmark
-        def ppf_Denmark(y1):
-            #calculates maximum y2 for a given y1, a1, a2, labor fixed at 1
-            y2 = a2_d*L_d - (a2_d/a1_d)*y1
-            return y2 if y2 >= 0 else 0.0  # ensure non-negative output level of the second sector
-
-        return np.vectorize(ppf_Denmark)  # return a vectorized version of ppf
     
-    def ricardian_ppf_Germany(self, a1_g, a2_g, L_d):
-        #production possibility set for Germany
-        def ppf_Germany(y1):
-            #calculates maximum y2 for a given y1, a1, a2, labor fixed at 1
-            y2 = a2_g*L_d - (a2_g/a1_g)*y1
-            return y2 if y2 >= 0 else 0.0  # ensure non-negative output level of the second sector
-
-        return np.vectorize(ppf_Germany)  # return a vectorized version of ppf
+    def ricardian_ppf(self, a1, a2, L):
+        def ppf(y1):
+            y2 = a2*L - (a2/a1)*y1
+            return y2
+        return np.vectorize(ppf)
 
     def plot_ppf(self, a1_d, a2_d, a1_g, a2_g, L_d, L_g):
 
-        ppf_d = self.ricardian_ppf_Denmark(a1_d, a2_d, L_d)
-        ppf_g = self.ricardian_ppf_Germany(a1_g, a2_g, L_g)
+        ppf_d = self.ricardian_ppf(a1_d, a2_d, L_d)
+        ppf_g = self.ricardian_ppf(a1_g, a2_g, L_g)
 
         # Plot the PPF curve
         y1_values = np.linspace(-2, 2, num=101)
