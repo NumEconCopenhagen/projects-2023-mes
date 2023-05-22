e6befe8360eaac765a5e05b209b5b3d3c342c526
wkz623 <wkz623@alumni.ku.dk>
2023-05-22 18:42:34 +0200

    almost final

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -83,24 +83,20 @@ class HouseholdSpecializationModelClass:
         # D.1 Plot the optimal ratios
         plt.plot(a, b, 'o--', label='Data Points')
 
-
         # D.2 Create heading, axis labels and legend
         plt.title('Exploring the Link Between Household Work and Gender Wage Disparity', fontsize=16)
         plt.xlabel('log(wF/wM)', fontsize=12)
         plt.ylabel('log(HF/HM)', fontsize=12)
         plt.legend()
 
-
         # D.3 Remove black box around the plot
         plt.gca().spines['top'].set_visible(False)
         plt.gca().spines['right'].set_visible(False)
-
 
         # D.4 Add data point values to the plot
         for i, (x, y) in enumerate(zip(a, b)):
             wf_value = par.wF_vec[i]
             plt.text(x, y, f'wF = {wf_value}', ha='left', va='bottom')
-
 
         # D.5 Show the plot
         plt.show()
@@ -130,7 +126,7 @@ class HouseholdSpecializationModelClass:
         I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
         u[I] = -np.inf
     
-         #d. find maximizing argument
+        # d. find maximizing argument
         j = np.argmax(u)
         
         opt.LM = LM[j]
@@ -171,10 +167,32 @@ class HouseholdSpecializationModelClass:
         sol.LM = res.x[0] 
         sol.LF = res.x[1] 
         sol.HM = res.x[2] 
-        sol.HF = res.x[3] # potentially change them in the direction of the utility function (LM, HM, LF, HF)
+        sol.HF = res.x[3]
 
-    
+
     # Question 4
+    def solve_wF_vec(self,discrete=False):
+        """ solve model for vector of female wages """
+
+
+        par = self.par
+        sol = self.sol
+
+
+        #loop over the vector of female wage and change the value of wF to whereever we are in the vector
+        for i, wF in enumerate(par.wF_vec):
+            self.par.wF = wF
+           
+            self.solve_continous()
+
+
+            #store the resulting values
+            sol.LM_vec[i] = sol.LM
+            sol.HM_vec[i] = sol.HM
+            sol.LF_vec[i] = sol.LF
+            sol.HF_vec[i] = sol.HF
+
+
     def run_regression(self):
         """ run regression """
 
@@ -186,8 +204,6 @@ class HouseholdSpecializationModelClass:
         A = np.vstack([np.ones(x.size),x]).T
         sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
 
-    
-    
     def estimate(self,alpha=None,sigma=None):
         """ estimate alpha and sigma """
 
@@ -234,92 +250,29 @@ class HouseholdSpecializationModelClass:
         par.alpha = res.x[0]
         par.sigma = res.x[1]
 
-    
-    def graphics_unlimited(self):
 
-        #create numpy arrays
-        X = np.array(placeholder_a)
-        Y = np.array(placeholder_s)
-        Z = np.array(placeholder_b)
+    # Question 5 - Continous Solver for the Extension
+    def objective_extension(self, x):
+        """ objective function to minimize """
+        LM, LF, HM, HF = x
+        return -self.calc_utility(LM, HM, LF, HF) # the minus is necessary to maximize within the minimize solver
 
-        #create "unlimited" figure
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_trisurf(X,Y,Z)
-        ax.scatter(0.982, 0.1, 0.4, color="red")
-        ax.view_init(30, 210)
-
-        plt.show()
-
-    
-    def graphics_limited(self):
-
-        #create numpy arrays
-        X = np.array(placeholder_a)
-        Y = np.array(placeholder_s)
-        Z = np.array(placeholder_b)
-
-        #create "unlimited" figure
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_trisurf(X,Y,Z)
-        ax.scatter(0.982, 0.1, 0.4, color="red")
-        ax.view_init(30, 210)
-        ax.set_zlim([0,1])
-
-        plt.show()
-
-
-
-    # Question 5
-
-    def estimate_fixed_alpha(self,sigma=None,alpha=None):
-        """ estimate alpha and sigma """
-
+    def solve_continous_extension(self, bound_female):
+        """ solve for optimal values of LM, LF, HM, HF """
         par = self.par
         sol = self.sol
 
-        #placeholder lists used later for graphic interpretation
-        #global placeholder_a
-        global placeholder_s
-        global placeholder_b
-        placeholder_a = []
-        placeholder_s = []
-        placeholder_b = []
+        # initial guess of results
+        x0 = [10 ,10, 10, 10]
 
-        # Objective Function to optimize alpha and sigma
-        def objective_function(x):
-            alpha, sigma = x
-            par.alpha = alpha
-            par.sigma = sigma
-            placeholder_HF = []
-            placeholder_HM = []
-            for wF in par.wF_vec:
-                par.wF = wF
-                self.solve_continous() 
-                placeholder_HF.append(sol.HF)
-                placeholder_HM.append(sol.HM)
-                placeholder_a.append(alpha)
-                placeholder_s.append(sigma)
-                placeholder_b.append(sol.beta0)
-            k = np.array(placeholder_HF)
-            l = np.array(placeholder_HM)
-            sol.HF_vec = k
-            sol.HM_vec = l
-            self.run_regression()
-            return (par.beta0_target - sol.beta0)**2 + (par.beta1_target - sol.beta1)**2
+        # predefine the bounds for the solutions
+        bounds = ((0, 24), (0, 24), (0, 24), (0, bound_female))
 
-        # Set narrow bounds for alpha and sigma
-        bounds = ((0.5, 0.5), (0.4, 1))
+        # creating the optimization function
+        res_extension = optimize.minimize(self.objective_extension, x0, bounds=bounds, method = 'Nelder-Mead' )
 
-        # Run the optimization
-        res = optimize.minimize(objective_function, x0=[0, 0], bounds=bounds, options={'eps':0.0001})
-
-        #store results
-        par.alpha = res.x[0]
-        par.sigma = res.x[1]
-        
-
-
-        
-    
+        # store the optimal results
+        sol.LM = res_extension.x[0] 
+        sol.LF = res_extension.x[1] 
+        sol.HM = res_extension.x[2] 
+        sol.HF = res_extension.x[3] 
